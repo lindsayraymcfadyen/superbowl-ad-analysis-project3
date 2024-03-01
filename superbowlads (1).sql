55cd6cac2656839d0318dc5420ff67c668e1f0e1
mattsturt1 <148403489+mattsturt1@users.noreply.github.com>
2024-02-29 21:33:16 -0600

    Add files via upload

---
+++ superbowlads (1).sql
@@ -0,0 +1,27 @@
+CREATE TABLE superBowlAds
+( year INT
+,brand VARCHAR
+,superbowl_ads_dot_com_url VARCHAR
+,youtube_url VARCHAR
+,funny BOOL
+,show_product_quickly BOOL
+,patriotic BOOL
+,celebrity BOOL
+,danger BOOL
+,animals BOOL
+,use_sex BOOL
+,id VARCHAR
+,kind VARCHAR
+,etag VARCHAR
+,view_count INT
+,like_count INT
+,dislike_count INT
+,favorite_count INT
+,comment_count INT
+,published_at DATE
+,title VARCHAR
+,description VARCHAR
+,thumbnail VARCHAR
+,channel_title VARCHAR
+,category_id INT
+);
